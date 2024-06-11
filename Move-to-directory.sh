4a788922b1d9d1a67269f507bdcc8f5bf879a962
koladeade <k.adepoju@alustudent.com>
2024-06-11 11:32:50 -0700

    files moved to directory

--- Move-to-directory.sh
+++ Move-to-directory.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-#function to move files to the directory
-mkdir -p "$negpod_9-q1"
-mv main.sh Students-list_1023.txt Select-emails.sh student-emails.txt "$negpod_9-q1/" 
+mv main.sh students-list_1023.txt Select-emails.sh student-emails.txt negpod_9-q1/
